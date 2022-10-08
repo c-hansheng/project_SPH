2fee2f144aeaa7c745bf43907c1266b66ce69f12
CHS <CHS_0001@163.com>
2022-10-08 23:07:11 +0800

    "修改header我的订单地址"

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -19,7 +19,7 @@
                         </p>
                     </div>
                     <div class="typeList">
-                        <a href="###">我的订单</a>
+                        <a href="/center">我的订单</a>
                         <router-link to="/shopCart" >我的购物车</router-link>
                         <a href="###">我的尚品汇</a>
                         <a href="###">尚品汇会员</a>
